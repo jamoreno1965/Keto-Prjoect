6e40caad2bd6a35f59e5d50341d632350e01314d
Michael Moreno <Morenomike100@gmail.com>
2019-01-11 14:57:37 -0800

    Update App.js

--- keto-project/src/App.js
+++ keto-project/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 class App extends Component { 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-       <h1 className="title">Keto Project</h1>
        <Navbar/>
-       <p></p>
-      <h4></h4>
+       <Route path='/' component={Home}/>     
       </div>
+      </BrowserRouter>
     );
   }
 }
